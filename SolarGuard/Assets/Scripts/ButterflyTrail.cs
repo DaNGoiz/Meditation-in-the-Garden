30edaf91258b4dbf17f12459f92cee0a41d64923
Izzy <3332099165@qq.com>
2024-04-13 00:40:13 +0800

    Try update main scene

---
+++ SolarGuard/Assets/Scripts/ButterflyTrail.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyTrail : MonoBehaviour
+{
+    // butterfly fly in a trail
+    // also need a script generate butterfly
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
